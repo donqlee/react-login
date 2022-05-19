201e709782bb0a749be4eefe070d4fe7e18a98a0
donqlee <donqlee88@gmail.com>
2022-05-19 23:22:46 +0900

    contextAPI2

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInformation = localStorage.getItem('isLoggedIn');
+        if(storedUserLoggedInformation === '1'){
+          setIsLoggedIn(true);
+        }
+      }, []); //의존성이 없을때는 처음 시작할때만 실행한다.
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = (email, password) => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    }
+    return <AuthContext.Provider
+        value={{isLoggedIn : isLoggedIn, onLogout: logoutHandler, onLogin : loginHandler}}
+    >{props.children}</AuthContext.Provider>
+}
 
 export default AuthContext;
